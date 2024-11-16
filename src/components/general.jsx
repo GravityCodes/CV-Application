1c70293020fd7d3efa4d3ee474d2db4c8f3b9892
Johan <johanotr@gmail.com>
2024-11-16 00:43:22 -0500

    refactor: add most components to resume component

--- src/components/general.jsx
+++ src/components/general.jsx
@@ -1,23 +1,20 @@
-export default function General() {
+
+
+export default function General({generalInfo, changeHandler}) { 
     return (
             <div>
                 <div className="input-field">
-                    <label htmlFor="first-name">First Name</label>
-                    <input type="text" id="first-name" />
-                </div>
-                <div className="input-field">
-                    <label htmlFor="last-name">Last Name</label>
-                    <input type="text" id="last-name" />
-                </div>
-                
+                    <label htmlFor="name">Full Name</label>
+                    <input type="text" id="name" value={generalInfo.name} onChange={changeHandler} />
+                </div>                
                 <div className="input-field">
                     <label htmlFor="email">Email</label>
-                    <input type="text" id="email" />
+                    <input type="text" id="email" value={generalInfo.email} onChange={changeHandler} />
                 </div>
     
                 <div className="input-field">
                     <label htmlFor="phone-number">Phone Number</label>
-                    <input type="tel" id="phone-number" />
+                    <input type="tel" id="phone-number" value={generalInfo.phone} onChange={changeHandler} />
                 </div>
             </div>
             )
--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -1,29 +1,59 @@
+import { useState } from 'react'
+import DropDown from './dropDown'
+import General from './general'
+import Address from './address'
+import Education from './education'
+import WorkExperience from './workExp'
+
 export default function  Resume(){
+    const [generalInfo, setGeneralInfo] = useState({name: "", email: "", phone: ""});
+    
+    function generalInfoHandler (e) {
+        
+        switch(e.target.id){
+            case "name": 
+                setGeneralInfo({...generalInfo, name: e.target.value});
+                break;
+            case "email": 
+                setGeneralInfo({...generalInfo, email: e.target.value});
+                break;
+            case "phone": 
+                setGeneralInfo({...generalInfo, phone: e.target.value});
+                break;
+        }
+    }
+    
     
     const resumeStyle = {
         padding: "15px 10px"
     }
 
     return (
-        <div className="resume-wrapper">
-            <div  style={resumeStyle} className="resume">
-                <h3>Your Name</h3>
-                <div className="profile-info">
-                    <p>Address</p>
-                    <p>Phone #</p>
-                    <p>Email</p>
+        <>
+            <DropDown name={"General Infomation"} info={<General generalInfo={generalInfo} changeHandler={generalInfoHandler} />}/>
+            <DropDown name={"Address"} info={<Address />}/>
+            <DropDown name={"Education"} info={<Education />}/>
+            <DropDown name={"Work Experience"} info={<WorkExperience />} />
+            <div className="resume-wrapper">
+                <div  style={resumeStyle} className="resume">
+                    <h3>{generalInfo.name == "" ? "Your Name": generalInfo.name}</h3>
+                    <div className="profile-info">
+                        <p>Boston MA 02121</p>
+                        <p>857-930-1820</p>
+                        <p>Johan.mesa2001@gmail.com</p>
+                    </div>
+
+
+                    <h4>Education</h4>
+                    <hr />
+
+                    <h4>Skills</h4>
+                    <hr />
+
+                    <h4>Work  Experience</h4>
+                    <hr />
                 </div>
-
-
-                <h4>Education</h4>
-                <hr />
-
-                <h4>Skills</h4>
-                <hr />
-
-                <h4>Work  Experience</h4>
-                <hr />
             </div>
-        </div>
+        </>
     )
 }
